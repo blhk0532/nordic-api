bdd06b298207d1eb9015141daa95ba98707ba2f2
blhk0532 <bjorn.leon.henry.kristoffersson@gmail.com>
2026-04-13 00:16:17 +0700

    adminos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -62,6 +62,7 @@
                     @auth
                         <a
                             href="/admin"
+                            style="display: none;"
                             class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
                         >
                             Dashboard
