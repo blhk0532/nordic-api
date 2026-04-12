37d214e262464e2b28dc4ecc105643b5bd174ee2
blhk0532 <bjorn.leon.henry.kristoffersson@gmail.com>
2026-04-13 00:22:13 +0700

    opt

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,7 +65,7 @@
                             style="display: none;"
                             class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
                         >
-                            Dashboard
+                    Dashboard
                         </a>
                     @else
                         <a
@@ -73,7 +73,7 @@
                             style="display: none;"
                             class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal hidden"
                         >
-                            Log in
+                    Log in
                         </a>
 
                         @if (Route::has('register'))
