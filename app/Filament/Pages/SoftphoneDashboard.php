336e96d101ba19dc3beb525b04d08f849f754b56
blhk0532 <bjorn.leon.henry.kristoffersson@gmail.com>
2026-04-22 06:39:17 +0700

    up

--- app/Filament/Pages/SoftphoneDashboard.php
+++ app/Filament/Pages/SoftphoneDashboard.php
@@ -3,8 +3,17 @@
 namespace App\Filament\Pages;
 
 use Filament\Pages\Page;
+use UnitEnum;
 
 class SoftphoneDashboard extends Page
 {
     protected string $view = 'filament.pages.softphone-dashboard';
+
+        protected static ?string $navigationLabel = 'Softphone';
+
+    protected static ?string $title = '';
+
+    protected static ?int $navigationSort = 5;
+
+    protected static UnitEnum|string|null $navigationGroup = 'Dialers TELE';
 }
